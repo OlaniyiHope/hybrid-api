88c9cc3f20415234ef17362667b8d5cdd4b391d0
OlaniyiHope <olaniyihoppee@gmail.com>
2022-10-31 21:49:54 +0100

    get hotel route i removed room

--- routes/hotels.js
+++ routes/hotels.js
@@ -5,12 +5,11 @@ import {
   createHotel,
   deleteHotel,
   getHotel,
-  getHotelRooms,
   getHotels,
   updateHotel,
 } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
-import {verifyAdmin} from "../utils/verifyToken.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 //CREATE
@@ -28,6 +27,5 @@ router.get("/find/:id", getHotel);
 router.get("/", getHotels);
 router.get("/countByCity", countByCity);
 router.get("/countByType", countByType);
-router.get("/room/:id", getHotelRooms);
 
 export default router;
