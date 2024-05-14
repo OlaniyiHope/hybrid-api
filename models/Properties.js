5c4012b17ff9e921630985d06fde5c80a4eaf71e
OlaniyiHope <olaniyihoppee@gmail.com>
2024-05-14 16:45:29 +0100

    all

--- models/Properties.js
+++ models/Properties.js
@@ -2,38 +2,30 @@ import mongoose from "mongoose";
 const Propertieschema = new mongoose.Schema({
   photos: {
     type: [String],
-    required: true,
   },
   name: {
     type: String,
-    required: true,
   },
   address: {
     type: String,
-    required: true,
   },
   city: {
     type: String,
-    required: true,
   },
 
   distance: {
     type: Number,
-    required: false,
   },
 
   detail: {
     type: String,
-    required: true,
   },
   desc: {
     type: String,
-    required: true,
   },
 
   cheapestPrice: {
-    type: Number,
-    required: true,
+    type: String,
   },
   featured: {
     type: Boolean,
